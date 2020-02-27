a2b3da3e44bf06fd94e0176cace818f0361443a4
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-02-27 15:14:21 +1100

    Switch to newer async/await syntax

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import asyncio
 from hachiko.hachiko import AIOWatchdog
 
-@asyncio.coroutine
-def watch_fs():
-    watch = AIOWatchdog('/Users/jbiesnecker/temp/forks/test')
+
+async def watch_fs():
+    watch = AIOWatchdog('/home/genevieve/Documents/Projects/watchdir/code/data/watch_dir/')
     watch.start()
     for _ in range(20):
-        yield from asyncio.sleep(1)
+        await asyncio.sleep(1)
     watch.stop()
 
 asyncio.get_event_loop().run_until_complete(watch_fs())
