348eec4e49875ee320002efda3710800b187819d
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-03-04 17:28:53 +1100

    Update example

---
+++ example.py
@@ -0,0 +1,30 @@
+import asyncio
+from hachiko.hachiko import AIOWatchdog, AIOEventHandler
+
+WATCH_DIRECTORY = '/path/to/watch/directory/'
+
+
+class MyEventHandler(AIOEventHandler):
+    """Subcalss of asyncio-compatible event handler."""
+    async def on_created(self, event):
+        print('Created:', event.src_path)  # add your functionality here
+
+    async def on_deleted(self, event):
+        print('Deleted:', event.src_path)  # add your functionality here
+
+    async def on_moved(self, event):
+        print('Moved:', event.src_path)  # add your functionality here
+
+    async def on_modified(self, event):
+        print('Modified:', event.src_path)  # add your functionality here
+
+
+async def watch_fs(watch_dir):
+    evh = MyEventHandler()
+    watch = AIOWatchdog(watch_dir, event_handler=evh)
+    watch.start()
+    for _ in range(20):
+        await asyncio.sleep(1)
+    watch.stop()
+
+asyncio.get_event_loop().run_until_complete(watch_fs(WATCH_DIRECTORY))
