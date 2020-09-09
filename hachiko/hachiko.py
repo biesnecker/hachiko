530fbfa2f8c39d1ce419380e4a39a527af3083dd
John Biesnecker <jbiesnecker@fb.com>
2020-09-09 15:58:08 -0700

    Remove unneeded array construction and loop

--- hachiko/hachiko.py
+++ hachiko/hachiko.py
@@ -1,10 +1,10 @@
 import asyncio
 from watchdog.observers import Observer
 
-EVENT_TYPE_MOVED = 'moved'
-EVENT_TYPE_DELETED = 'deleted'
-EVENT_TYPE_CREATED = 'created'
-EVENT_TYPE_MODIFIED = 'modified'
+EVENT_TYPE_MOVED = "moved"
+EVENT_TYPE_DELETED = "deleted"
+EVENT_TYPE_CREATED = "created"
+EVENT_TYPE_MODIFIED = "modified"
 
 
 class AIOEventHandler(object):
@@ -13,39 +13,40 @@ class AIOEventHandler(object):
     def __init__(self, loop=None):
         self._loop = loop or asyncio.get_event_loop()
         # prefer asyncio.create_task starting from Python 3.7
-        if hasattr(asyncio, 'create_task'):
+        if hasattr(asyncio, "create_task"):
             self._ensure_future = asyncio.create_task
         else:
             self._ensure_future = asyncio.ensure_future
-
-    async def on_any_event(self, event): pass
-
-    async def on_moved(self, event): pass
-
-    async def on_created(self, event): pass
-
-    async def on_deleted(self, event): pass
-
-    async def on_modified(self, event): pass
-
-    def dispatch(self, event):
-        _method_map = {
+        self._method_map = {
             EVENT_TYPE_MODIFIED: self.on_modified,
             EVENT_TYPE_MOVED: self.on_moved,
             EVENT_TYPE_CREATED: self.on_created,
             EVENT_TYPE_DELETED: self.on_deleted,
         }
-        handlers = [self.on_any_event, _method_map[event.event_type]]
-        for handler in handlers:
-            self._loop.call_soon_threadsafe(
-                self._ensure_future,
-                handler(event))
+
+    async def on_any_event(self, event):
+        pass
+
+    async def on_moved(self, event):
+        pass
+
+    async def on_created(self, event):
+        pass
+
+    async def on_deleted(self, event):
+        pass
+
+    async def on_modified(self, event):
+        pass
+
+    def dispatch(self, event):
+        handler = self._method_map[event.event_type]
+        self._loop.call_soon_threadsafe(self._ensure_future, self.on_any_event(event))
+        self._loop.call_soon_threadsafe(self._ensure_future, handler(event))
 
 
 class AIOWatchdog(object):
-
-    def __init__(self, path='.', recursive=True, event_handler=None,
-                 observer=None):
+    def __init__(self, path=".", recursive=True, event_handler=None, observer=None):
         if observer is None:
             self._observer = Observer()
         else:
