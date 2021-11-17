627ee63083dcbe7248d38926520a6a58d8d89448
John Biesnecker <jbiesnecker@fb.com>
2021-11-17 09:56:44 -0800

    Bump version and minor cleanup

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 import os
-import sys
 from distutils.core import setup
+from hachiko.version import __version__
+
 
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-if sys.version_info >= (3, 5):
-    install_requires = []
-else:
-    install_requires = ['asyncio']
 
-install_requires.append('watchdog')
+exec(open("hachiko/version.py").read())
 
-exec(open('hachiko/version.py').read())
-
-setup(name='hachiko',
-      version=__version__,
-      author='John Biesnecker',
-      author_email='jbiesnecker@gmail.com',
-      url='https://github.com/biesnecker/hachiko',
-      packages=['hachiko'],
-      package_dir={'hachiko': './hachiko'},
-      install_requires=install_requires,
-      description='Asyncio wrapper around watchdog.',
-      license='mit',
-      long_description=read('README.txt')
+setup(
+    name="hachiko",
+    version=__version__,
+    author="John Biesnecker",
+    author_email="jbiesnecker@gmail.com",
+    url="https://github.com/biesnecker/hachiko",
+    packages=["hachiko"],
+    package_dir={"hachiko": "./hachiko"},
+    install_requires=["watchdog"],
+    description="Asyncio wrapper around watchdog.",
+    license="mit",
+    long_description=read("README.txt"),
 )
